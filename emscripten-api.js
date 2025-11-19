f32bf0e89fcd6613055e4d9e894b6c043ceee908
Carl Woffenden <cwoffenden@gmail.com>
2025-11-19 18:05:31 +0100

    Working test

--- emscripten-api.js
+++ emscripten-api.js
@@ -1,10 +1,11 @@
 /*
- * Pure JS implementation of the required bits of Emscripten's API.
+ * Pure JS implementation of the required bits of Emscripten's API (a copy and
+ * paste where possible).
  *
- * Functions expect a HEAPU32 in scope.
+ * Functions expect a HEAPU32 and HEAP32 in scope.
  */
- 
- // assert.h
+
+// assert.h
 
 export function assert(val) {
 	console.assert(val);
@@ -22,6 +23,14 @@ export function emscripten_get_now() {
 		return Date.now();
 	}
 }
+
+// emscripten/console.h
+
+export function emscripten_out(str) {
+	console.log("%s", str);
+}
+
+export var emscripten_outf = console.log;
 
 // emscripten/atomic.h
 
@@ -45,26 +54,26 @@ export function emscripten_lock_init(addr) {
 
 export function emscripten_lock_busyspin_wait_acquire(addr, maxWaitMilliseconds) {
 	var val = emscripten_atomic_cas_u32(addr, 0, 1);
-	if (!val) return true;
+	if (!val) return 1;
 
 	var t = emscripten_get_now();
 	var waitEnd = t + maxWaitMilliseconds;
 	while (t < waitEnd) {
 		val = emscripten_atomic_cas_u32(addr, 0, 1);
-		if (!val) return true;
+		if (!val) return 1;
 		t = emscripten_get_now();
 	}
-	return false;
+	return 0;
 }
 
 export function emscripten_lock_try_acquire(addr) {
 	var val = emscripten_atomic_cas_u32(addr, 0, 1);
-	return !val;
+	return (!val) ? 1 : 0;
 }
 
 export function emscripten_lock_release(addr) {
 	Atomics.store(HEAPU32, addr >> 2, 0);
-	Atomics.notify(HEAPU32, addr >> 2, 1); //<-- left as-is, even though we're spinning not waiting
+	Atomics.notify(HEAP32, addr >> 2, 1); //<-- left as-is, even though we're spinning not waiting
 }
 
 // emscripten/eventloop.h
--- test-processor.js
+++ test-processor.js
@@ -1,10 +1,8 @@
 import {
 	assert,
-	emscripten_get_now,
-	emscripten_atomic_cas_u32,
+	emscripten_outf,
 	emscripten_atomic_load_u32,
 	emscripten_atomic_store_u32,
-	emscripten_lock_init,
 	emscripten_lock_busyspin_wait_acquire,
 	emscripten_lock_try_acquire,
 	emscripten_lock_release
@@ -14,17 +12,59 @@ class TestProcessor extends AudioWorkletProcessor {
 	constructor(args) {
 		super();
 		if (args && args.processorOptions) {
+			// Push vars into the AWP's scope
 			for (var opt in args.processorOptions) {
 				globalThis[opt] = args.processorOptions[opt];
 			}
+			// Then signal to the main thread to start the test
 			emscripten_atomic_store_u32(whichTest, Test.TEST_NOT_STARTED);
 		}
 		this.port.onmessage = (e) => {
-			console.log(e.data);
+			emscripten_outf("AWP message: %s", e.data);
 		};
 	}
 
 	process(inputs, outputs, params) {
+		var result = 0;
+		switch (emscripten_atomic_load_u32(whichTest)) {
+		case Test.TEST_TRY_ACQUIRE:
+			// Was locked after init, should fail to acquire
+			result = emscripten_lock_try_acquire(testLock);
+			emscripten_outf("TEST_TRY_ACQUIRE: %d (expect: 0)", result);
+			assert(!result);
+			emscripten_atomic_store_u32(whichTest, Test.TEST_WAIT_ACQUIRE_FAIL);
+			break;
+		case Test.TEST_WAIT_ACQUIRE_FAIL:
+			// Still locked so we fail to acquire
+			result = emscripten_lock_busyspin_wait_acquire(testLock, 100);
+			emscripten_outf("TEST_WAIT_ACQUIRE_FAIL: %d (expect: 0)", result);
+			assert(!result);
+			emscripten_atomic_store_u32(whichTest, Test.TEST_WAIT_ACQUIRE);
+    		/*
+    		 * Fall through here so the worker/main has a chance to unlock
+    		 * whilst spinning (otherwise the lock has a chance to be released
+    		 * before spinning).
+    		 */
+    	case Test.TEST_WAIT_ACQUIRE:
+    		// Will get unlocked in worker/main, so should quickly acquire
+    		result = emscripten_lock_busyspin_wait_acquire(testLock, 1000);
+    		emscripten_outf("TEST_WAIT_ACQUIRE: %d  (expect: 1)", result);
+    		assert(result);
+			emscripten_atomic_store_u32(whichTest, Test.TEST_RELEASE);
+			break;
+		case Test.TEST_RELEASE:
+			// Unlock, check the result
+			emscripten_lock_release(testLock);
+			result = emscripten_lock_try_acquire(testLock);
+			emscripten_outf("TEST_RELEASE: %d (expect: 1)", result);
+			assert(result);
+			emscripten_atomic_store_u32(whichTest, Test.TEST_DONE);
+			break;
+		case Test.TEST_DONE:
+			return false;
+		default:
+			break;
+		}
 		return true;
 	}
 }
