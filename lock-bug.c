37cda6e554c0bace451ec10d9502c52a602f61b8
Carl Woffenden <cwoffenden@gmail.com>
2025-11-28 15:30:46 +0100

    Copy back of the JS code

--- lock-bug.c
+++ lock-bug.c
@@ -13,14 +13,23 @@ int _emscripten_thread_supports_atomics_wait(void);
 #endif
 
 typedef enum {
-  TEST_LOADING,           // The page is still loading
-  TEST_NOT_STARTED,       // The test hasn't yet started
-  TEST_TRY_ACQUIRE,       // Acquired in main, fail in process()
-  TEST_WAIT_ACQUIRE_FAIL, // Keep acquired so time-out
-  TEST_WAIT_ACQUIRE,      // Release in main, succeed in process()
-  TEST_RELEASE,           // Release in process() after above
-  TEST_DONE               // Test finished
+	TEST_LOADING,           // The page is still loading
+	TEST_NOT_STARTED,       // The test hasn't yet started
+	TEST_TRY_ACQUIRE,       // Acquired in main, fail in process()
+	TEST_WAIT_ACQUIRE_FAIL, // Keep acquired so time-out
+	TEST_WAIT_ACQUIRE,      // Release in main, succeed in process()
+	TEST_RELEASE,           // Release in process() after above
+	TEST_DONE               // Test finished
 } Test;
+
+typedef enum {
+	BROWSER_UNKNOWN = 0, // Unknown or unable to determine
+	BROWSER_FIREFOX = 1, // Firefox (plus Firefox-derived, such as Iceweasel)
+	BROWSER_CHROME  = 2, // Chrome (desktop, Chromebook and mobile, plus Chrome-derived, such as Opera)
+	BROWSER_SAFARI  = 3, // Safari (desktop and mobile)
+	BROWSER_IE      = 4, // IE11 (and earlier if WebGL is not required)
+	BROWSER_EDGE    = 5, // Edge (the original pre-2019 legacy Edge)
+} Browser;
 
 // Style for the mainLoop log (magenta)
 const char* STYLE_MAIN = "\x1B[95m";
@@ -36,6 +45,8 @@ Test* const whichTest = &(Test){};
 double startTime = 0;
 // Has TEST_WAIT_ACQUIRE unlocked testLock?
 bool waitAcquireUnlocked = false;
+// How many runs through has this done?
+unsigned runCount = 0;
 
 // Global audio context
 EMSCRIPTEN_WEBAUDIO_T audioContext = 0;
@@ -44,23 +55,57 @@ EMSCRIPTEN_AUDIO_WORKLET_NODE_T workletNode = 0;
 // AW stack
 char* workletStack[1024];
 
+// Borrowed from Xplat (overkill but the order counts)
+EM_JS(Browser, getBrowser, (), {
+	if (navigator) {
+		var ua = String(navigator.userAgent);
+		if (/Firefox\\\\/\\\\d+\\\\.\\\\d+/.test(ua)) {
+			return /*BROWSER_FIREFOX*/ 1;
+		} else {
+			if (/Edge\\\\/\\\\d+\\\\.\\\\d+/.test(ua)) {
+				return /*BROWSER_EDGE*/ 5;
+			} else {
+				if (/^((?!Chrome).)*Safari\\\\/\\\\d+\\\\.\\\\d+/.test(ua)) {
+					return /*BROWSER_SAFARI*/ 3;
+				} else {
+					if (/Chrome\\\\/\\\\d+\\\\.\\\\d+/.test(ua)) {
+						return /*BROWSER_CHROME*/ 2;
+					} else {
+						if (/Trident\\\\/\\\\d+\\\\.\\\\d+/.test(ua)) {
+							return /*BROWSER_IE*/ 4;
+						}
+					}
+				}
+			}
+		}
+	}
+	return /*BROWSER_UNKNOWN*/ 0;
+});
+
+// Missing from console.h
+EM_JS(void, console_clear, (), {
+	console.clear();
+});
+
 // AW callback (called approx 375x per second)
 bool process(int numInputs, const AudioSampleFrame *inputs, int numOutputs, AudioSampleFrame *outputs, int numParams, const AudioParamFrame *params, void *userData) {
 	emscripten_outf("%s*** enter process()", STYLE_PROC);
-	assert(emscripten_current_thread_is_audio_worklet()
-		&& !_emscripten_thread_supports_atomics_wait());
 	bool runAgain = true;
 	int result;
 	double waitMs;
 	switch (emscripten_atomic_load_u32(whichTest)) {
 	case TEST_LOADING:
+		assert(emscripten_current_thread_is_audio_worklet()
+			&& !_emscripten_thread_supports_atomics_wait());
 		// AWP has been loaded, tell the main thread
-		emscripten_outf("%sTEST_LOADING", STYLE_PROC);
+		emscripten_outf("%sTEST_LOADING (is AW: %d, has wait: %d, expect: 1 & 0)",
+			STYLE_PROC, emscripten_current_thread_is_audio_worklet(),
+				_emscripten_thread_supports_atomics_wait());
 		emscripten_atomic_store_u32(whichTest, TEST_NOT_STARTED);
 		break;
 	case TEST_NOT_STARTED:
 		// Waiting on the main thread to acknowledge
-		emscripten_outf("%sTEST_NOT_STARTED", STYLE_PROC);
+		emscripten_outf("%sTEST_NOT_STARTED (may spam a lot)", STYLE_PROC);
 		break;
 	case TEST_TRY_ACQUIRE:
 		// Was locked after init, should fail to acquire
@@ -78,12 +123,12 @@ bool process(int numInputs, const AudioSampleFrame *inputs, int numOutputs, Audi
 		emscripten_outf("%sTEST_WAIT_ACQUIRE_FAIL: %d (expect: 0, spinning for %dms)", STYLE_PROC, result, (int) waitMs);
 		assert(!result);
 		emscripten_atomic_store_u32(whichTest, TEST_WAIT_ACQUIRE);
-    	/*
+		/*
 		 * Fall through here so the main has a chance to unlock whilst
 		 * spinning (otherwise the lock has a chance to be released before
 		 * spinning).
 		 */
-    case TEST_WAIT_ACQUIRE:
+	case TEST_WAIT_ACQUIRE:
 		// Will get unlocked in the main, so should quickly acquire
 		emscripten_outf("%sTEST_WAIT_ACQUIRE: start spinning!", STYLE_PROC);
 		waitMs = emscripten_get_now();
@@ -101,11 +146,11 @@ bool process(int numInputs, const AudioSampleFrame *inputs, int numOutputs, Audi
 		result = emscripten_lock_try_acquire(testLock);
 		emscripten_outf("%sTEST_RELEASE: %d (expect: 1)", STYLE_PROC, result);
 		assert(result);
-		emscripten_atomic_store_u32(whichTest, TEST_DONE);
+		emscripten_atomic_store_u32(whichTest, TEST_NOT_STARTED);
 		break;
 	default:
 		// Finished, exit from the audio thread
-		emscripten_outf("%sTEST_DONE in process()", STYLE_PROC);
+		emscripten_outf("%sTEST_DONE in process", STYLE_PROC);
 		runAgain = false;
 	}
 	emscripten_outf("%s*** exit process()", STYLE_PROC);
@@ -115,8 +160,6 @@ bool process(int numInputs, const AudioSampleFrame *inputs, int numOutputs, Audi
 // Called every 10ms-ish using a timeout
 bool mainLoop(double time, void* data) {
 	emscripten_outf("%s*** enter mainLoop()", STYLE_MAIN);
-	assert(!emscripten_current_thread_is_audio_worklet()
-		&& !_emscripten_thread_supports_atomics_wait());
 	bool runAgain = true;
 	switch (emscripten_atomic_load_u32(whichTest)) {
 	case TEST_LOADING:
@@ -124,15 +167,24 @@ bool mainLoop(double time, void* data) {
 		emscripten_outf("%sTEST_LOADING: Still loading", STYLE_MAIN);
 		break;
 	case TEST_NOT_STARTED:
+		assert(!emscripten_current_thread_is_audio_worklet()
+			&& !_emscripten_thread_supports_atomics_wait());
+		// Periodically clear the console (it gets sluggish otherwise)
+		if (runCount > 0 && (runCount & 63) == 0) {
+			console_clear();
+		}
 		// The AWP has loaded, so move to the first test
-		emscripten_outf("%sTEST_NOT_STARTED: Staring test", STYLE_MAIN);
+		emscripten_outf("%sTEST_NOT_STARTED: Staring test (is AW: %d, has wait: %d, expect 0 & 0)",
+			STYLE_MAIN, emscripten_current_thread_is_audio_worklet(),
+				_emscripten_thread_supports_atomics_wait());
+		waitAcquireUnlocked = false;
 		emscripten_atomic_store_u32(whichTest, TEST_TRY_ACQUIRE);
 		break;
 	case TEST_TRY_ACQUIRE:
 		emscripten_outf("%sTEST_TRY_ACQUIRE: main nothing to do (waiting on AW starting)", STYLE_MAIN);
 		break;
 	case TEST_WAIT_ACQUIRE_FAIL:
-		emscripten_outf("%sTEST_WAIT_ACQUIRE_FAIL: main nothing to do", STYLE_MAIN);
+		emscripten_outf("%sTEST_WAIT_ACQUIRE_FAIL: main waiting out 100ms", STYLE_MAIN);
 		break;
 	case TEST_WAIT_ACQUIRE:
 		if (!waitAcquireUnlocked) {
@@ -141,6 +193,7 @@ bool mainLoop(double time, void* data) {
 			emscripten_lock_release(testLock);
 			emscripten_outf("%sTEST_WAIT_ACQUIRE: main lock released", STYLE_MAIN);
 			waitAcquireUnlocked = true;
+			runCount++;
 		} else {
 			emscripten_outf("%sTEST_WAIT_ACQUIRE: main already unlocked", STYLE_MAIN);
 		}
@@ -150,7 +203,8 @@ bool mainLoop(double time, void* data) {
 		break;
 	default:
 		// Finished, exit from the main thread
-		emscripten_outf("%sTEST_DONE: %dms (expect: > 100)", STYLE_MAIN, (int) (emscripten_get_now() - startTime));
+		emscripten_outf("%sTEST_DONE: %dms (expect: > 100, runs: %d)", STYLE_MAIN,
+			(int) (emscripten_get_now() - startTime), runCount);
 		runAgain = false;
 	}
 	emscripten_outf("%s*** exit mainLoop()", STYLE_MAIN);
@@ -186,6 +240,11 @@ KEEP_IN_MODULE void runTest() {
 		emscripten_out("This was designed to run once, reload the page");
 		return;
 	}
+	if (getBrowser() != BROWSER_CHROME) {
+		// No ANSI colours if this isn't Chrome
+		STYLE_MAIN = "";
+		STYLE_PROC = "\t";
+	}
 	/*
 	 * The basics: do locks work?
 	 */
@@ -209,21 +268,33 @@ KEEP_IN_MODULE void runTest() {
 	emscripten_set_timeout_loop(mainLoop, 10, NULL);
 }
 
+// Very dirty stop (mostly to read the log)
+KEEP_IN_MODULE void stopTest() {
+	emscripten_out("Stopping test, ignore any errors");
+	emscripten_atomic_store_u32(whichTest, TEST_DONE);
+}
+
 // Adds the same UI to the page as the JS version
 EM_JS(void, addUI, (), {
 	var label = document.createElement("p");
 	label.appendChild(document.createTextNode("Click to start the test, results are in the console."));
 	document.body.appendChild(label);
-	var button = document.createElement("button");
-	button.appendChild(document.createTextNode("Start Test"));
-	button.onclick = () => {
-		if (_runTest) {
+	var button1 = document.createElement("button");
+	button1.appendChild(document.createTextNode("Start Test"));
+	button1.onclick = () => {
+		if (globalThis._runTest) {
 			_runTest();
-		} else {
-			console.error("")
 		}
 	};
-	document.body.appendChild(button);
+	document.body.appendChild(button1);
+	var button2 = document.createElement("button");
+	button2.appendChild(document.createTextNode("Stop Test"));
+	button2.onclick = () => {
+		if (globalThis._stopTest) {
+			_stopTest();
+		}
+	};
+	document.body.appendChild(button2);
 });
 
 int main() {
